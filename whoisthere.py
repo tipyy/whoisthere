7a71c6523afa0daa50dd26d6ea04f46f77518865
tontonDuPirox <onclebobs@gmail.com>
2013-03-09 14:17:02 +0100

    Update whoisthere.py

--- whoisthere.py
+++ whoisthere.py
@@ -45,3 +45,63 @@ def mygetopts():
             MAIL = False
         if opt[0] == '-f':
             whitelistFileUri = opt[1]
+if opt[0] == '-h':
+            Usage()
+        if opt[0] == '-l':
+            LOOP_ENABLED = True
+            LOOP_WAIT_TIME = opt[1]
+
+#let's go !
+try:
+    mygetopts()
+    print time.strftime("%d/%m/%Y %H:%M") 
+    #getting the whitelist from file
+    whitelist = []
+    whitelistFile = open(whitelistFileUri, "r")
+    for line in whitelistFile:
+        if line[0] != "#":
+            whitelist.append(string.split(line.strip('\n'), " "))
+    whitelistFile.close()
+
+    #run a 'nmap' command and assign results to a variable
+    #commandString = "nmap -T4 -sP 192.168.0.* | grep -v ^$ | grep -v Nmap | grep -v " + me
+    commandString = "nmap -T4 -sP 192.168.0.* | grep -v ^$ | grep -v Nmap"
+    commandOutput = commands.getoutput(commandString)
+    findResults = string.split(commandOutput, "\n")
+
+    #getting hosts list with mac addresses
+    hosts = []
+    for result in findResults:
+        splittedResult = string.split(result, " ")
+        if result[0:4] == "Host":
+            host = []
+            host.append(splittedResult[1]) 
+        elif result[0:12] == "MAC Address:":      
+            host.append(splittedResult[2]) 
+            hosts.append(host)
+
+    #display
+    print "=================================================================="
+    for daHost in hosts:
+        print "Machine : " + daHost[0] + " Adresse MAC : " + daHost[1]
+        finded = False
+        whitelistName = ""
+        for w in whitelist:
+            if daHost[1].upper() in w:
+finded = True
+                whitelistName = w[1]
+        if finded:
+            print "ok c'est " + whitelistName
+        else:
+            print "un intrus"
+            print "payes ton scan sur " + daHost[0]
+            commandString = "nmap -A " + daHost[0]
+            commandOutput = commands.getoutput(commandString)
+            print commandOutput
+            if MAIL:
+                print "On envoi le mail avec le resultat du scan"
+                send(commandOutput)
+        print "=================================================================="
+except Exception as e:
+    print "On a eu un petit soucis"
+    print e
